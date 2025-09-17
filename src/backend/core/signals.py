8abe74f44c743dd9554225b2f24ae390862fce8c
Fabre Florian <ffabre@hybird.org>
2025-09-17 07:47:15 +0200

    ✨(backend) add fallback search & default ordering
    Filter deleted documents from visited ones.
    Set default ordering to the Find API search call (-updated_at)
    BaseDocumentIndexer.search now returns a list of document ids instead of models.
    Do not call the indexer in signals when SEARCH_INDEXER_CLASS is not defined
    or properly configured.
    
    Signed-off-by: Fabre Florian <ffabre@hybird.org>

--- src/backend/core/signals.py
+++ src/backend/core/signals.py
@@ -2,10 +2,14 @@
 Declare and configure the signals for the impress core application
 """
 
+from functools import partial
+
+from django.db import transaction
 from django.db.models import signals
 from django.dispatch import receiver
 
 from . import models
+from .services.search_indexers import default_document_indexer
 from .tasks.find import trigger_document_indexer
 
 
@@ -16,7 +20,8 @@ def document_post_save(sender, instance, **kwargs):  # pylint: disable=unused-ar
     Note : Within the transaction we can have an empty content and a serialization
     error.
     """
-    trigger_document_indexer(instance, on_commit=True)
+    if default_document_indexer() is not None:
+        transaction.on_commit(partial(trigger_document_indexer, instance))
 
 
 @receiver(signals.post_save, sender=models.DocumentAccess)
@@ -24,5 +29,5 @@ def document_access_post_save(sender, instance, created, **kwargs):  # pylint: d
     """
     Asynchronous call to the document indexer at the end of the transaction.
     """
-    if not created:
-        trigger_document_indexer(instance.document, on_commit=True)
+    if not created and default_document_indexer() is not None:
+        transaction.on_commit(partial(trigger_document_indexer, instance.document))
--- src/backend/core/tests/documents/test_api_documents_search.py
+++ src/backend/core/tests/documents/test_api_documents_search.py
@@ -2,12 +2,15 @@
 Tests for Documents API endpoint in impress's core app: list
 """
 
+from json import loads as json_loads
+
 import pytest
 import responses
 from faker import Faker
 from rest_framework.test import APIClient
 
 from core import factories, models
+from core.services.search_indexers import default_document_indexer
 
 fake = Faker()
 pytestmark = pytest.mark.django_db
@@ -16,13 +19,12 @@ pytestmark = pytest.mark.django_db
 @pytest.mark.parametrize("role", models.LinkRoleChoices.values)
 @pytest.mark.parametrize("reach", models.LinkReachChoices.values)
 @responses.activate
-def test_api_documents_search_anonymous(reach, role, settings):
+def test_api_documents_search_anonymous(reach, role, indexer_settings):
     """
     Anonymous users should not be allowed to search documents whatever the
     link reach and link role
     """
-    factories.DocumentFactory(link_reach=reach, link_role=role)
-    settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
+    indexer_settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
 
     factories.DocumentFactory(link_reach=reach, link_role=role)
 
@@ -45,46 +47,90 @@ def test_api_documents_search_anonymous(reach, role, settings):
     }
 
 
-def test_api_documents_search_endpoint_is_none(settings):
-    """Missing SEARCH_INDEXER_QUERY_URL should throw an error"""
-    settings.SEARCH_INDEXER_QUERY_URL = None
+def test_api_documents_search_endpoint_is_none(indexer_settings):
+    """
+    Missing SEARCH_INDEXER_QUERY_URL, so the indexer is not properly configured.
+    Should fallback on title filter
+    """
+    indexer_settings.SEARCH_INDEXER_QUERY_URL = None
+
+    assert default_document_indexer() is None
 
     user = factories.UserFactory()
+    document = factories.DocumentFactory(title="alpha")
+    access = factories.UserDocumentAccessFactory(document=document, user=user)
 
     client = APIClient()
     client.force_login(user)
 
-    response = APIClient().get("/api/v1.0/documents/search/", data={"q": "alpha"})
+    response = client.get("/api/v1.0/documents/search/", data={"q": "alpha"})
 
-    assert response.status_code == 401
-    assert response.json() == {"detail": "The service is not properly configured."}
+    assert response.status_code == 200
+    content = response.json()
+    results = content.pop("results")
+    assert content == {
+        "count": 1,
+        "next": None,
+        "previous": None,
+    }
+    assert len(results) == 1
+    assert results[0] == {
+        "id": str(document.id),
+        "abilities": document.get_abilities(user),
+        "ancestors_link_reach": None,
+        "ancestors_link_role": None,
+        "computed_link_reach": document.computed_link_reach,
+        "computed_link_role": document.computed_link_role,
+        "created_at": document.created_at.isoformat().replace("+00:00", "Z"),
+        "creator": str(document.creator.id),
+        "depth": 1,
+        "excerpt": document.excerpt,
+        "link_reach": document.link_reach,
+        "link_role": document.link_role,
+        "nb_accesses_ancestors": 1,
+        "nb_accesses_direct": 1,
+        "numchild": 0,
+        "path": document.path,
+        "title": document.title,
+        "updated_at": document.updated_at.isoformat().replace("+00:00", "Z"),
+        "user_role": access.role,
+    }
 
 
 @responses.activate
-def test_api_documents_search_invalid_params(settings):
+def test_api_documents_search_invalid_params(indexer_settings):
     """Validate the format of documents as returned by the search view."""
-    settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
+    indexer_settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
 
     user = factories.UserFactory()
 
     client = APIClient()
     client.force_login(user)
 
-    response = APIClient().get("/api/v1.0/documents/search/")
+    response = client.get("/api/v1.0/documents/search/")
 
     assert response.status_code == 400
     assert response.json() == {"q": ["This field is required."]}
 
-    response = APIClient().get("/api/v1.0/documents/search/", data={"q": "    "})
+    response = client.get("/api/v1.0/documents/search/", data={"q": "    "})
 
     assert response.status_code == 400
     assert response.json() == {"q": ["This field may not be blank."]}
 
+    response = client.get(
+        "/api/v1.0/documents/search/", data={"q": "any", "page": "NaN"}
+    )
+
+    assert response.status_code == 400
+    assert response.json() == {"page": ["A valid integer is required."]}
+
 
 @responses.activate
-def test_api_documents_search_format(settings):
+def test_api_documents_search_format(indexer_settings):
     """Validate the format of documents as returned by the search view."""
-    settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
+    indexer_settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
+
+    assert default_document_indexer() is not None
 
     user = factories.UserFactory()
 
@@ -139,4 +185,50 @@ def test_api_documents_search_format(settings):
         "title": document.title,
         "updated_at": document.updated_at.isoformat().replace("+00:00", "Z"),
         "user_role": access.role,
+    }
+
+
+@responses.activate
+def test_api_documents_search_pagination(indexer_settings):
+    """Documents should be ordered by descending "updated_at" by default"""
+    indexer_settings.SEARCH_INDEXER_QUERY_URL = "http://find/api/v1.0/search"
+
+    assert default_document_indexer() is not None
+
+    user = factories.UserFactory()
+
+    client = APIClient()
+    client.force_login(user)
+
+    docs = factories.DocumentFactory.create_batch(10)
+
+    # Find response
+    # pylint: disable-next=assignment-from-none
+    api_search = responses.add(
+        responses.POST,
+        "http://find/api/v1.0/search",
+        json=[{"_id": str(doc.pk)} for doc in docs],
+        status=200,
+    )
+
+    response = client.get(
+        "/api/v1.0/documents/search/", data={"q": "alpha", "page": 2, "page_size": 5}
+    )
+
+    assert response.status_code == 200
+    content = response.json()
+    results = content.pop("results")
+    assert len(results) == 5
+
+    # Check the query parameters.
+    assert api_search.call_count == 1
+    assert api_search.calls[0].response.status_code == 200
+    assert json_loads(api_search.calls[0].request.body) == {
+        "q": "alpha",
+        "visited": [],
+        "services": ["docs"],
+        "page_number": 2,
+        "page_size": 5,
+        "order_by": "updated_at",
+        "order_direction": "desc",
     }
